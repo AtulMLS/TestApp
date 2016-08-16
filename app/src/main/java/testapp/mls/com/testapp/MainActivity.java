9ec5f8e3d1b244d3a49098db593039bc8da51231
Atul <atul.mlogicsol@gmail.com>
2016-08-16 14:46:55 +0530

    Toast changed

--- app/src/main/java/testapp/mls/com/testapp/MainActivity.java
+++ app/src/main/java/testapp/mls/com/testapp/MainActivity.java
@@ -5,9 +5,10 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own other action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
-
+                Toast.makeText(MainActivity.this, "Toast data ffffffffffff", Toast.LENGTH_SHORT).show();
             }
         });
     }
