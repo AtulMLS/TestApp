617b599e1a38ff3842ca51e109d02d7c6d4de462
Atul <atul.mlogicsol@gmail.com>
2016-08-16 13:36:12 +0530

    Toast removed

--- app/src/main/java/testapp/mls/com/testapp/MainActivity.java
+++ app/src/main/java/testapp/mls/com/testapp/MainActivity.java
@@ -5,10 +5,9 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own other action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
-                Toast.makeText(MainActivity.this, "Replace with your own other action", Toast.LENGTH_SHORT).show();
+
             }
         });
     }
